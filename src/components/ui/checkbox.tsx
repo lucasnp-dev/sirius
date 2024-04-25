5c7192f602ff3c0111e6ac630c3521bb36526e55
Lucas Nunes <lucasnp.dev@gmail.com>
2024-04-25 17:05:07 -0300

    C3 - Data Table

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,4 +1,4 @@
-// @ts-expect-error checkbox
+// TODO: react-icons
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox'
 import { Check } from 'lucide-react'
 import * as React from 'react'
